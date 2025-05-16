8ef034653fe3cade03f3dda85bb6d9bcd856c89e
Luka P <lukapavicevic5@gmail.com>
2025-05-16 17:49:18 +0200

    Change Download script so they are more modular.

--- data/download-data.py
+++ data/download-data.py
@@ -1,46 +1,55 @@
-import requests
-import zipfile
-import os
+import requests, os, zipfile, tqdm
 
 def unzip_and_delete(file_path):
-    
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
         zip_ref.extractall(file_path.split(".")[0])
-    
+
     os.remove(file_path)
 
-# Download all the data
-print("Downloading Weather data!")
-weather_api = "https://archive-api.open-meteo.com/v1/archive?latitude=40.73061&longitude=-73.935242&start_date=2020-01-01&end_date=2025-01-01&hourly=temperature_2m,rain,snowfall,snow_depth,wind_speed_10m,wind_gusts_10m&timezone=America%2FNew_York&format=csv"
-reposne = requests.get(weather_api)
-with open("weather-data.csv", 'wb') as f:
-    f.write(reposne.content)
+def download_from_api(api_data):
+    reposne = requests.get(api_data["url"])
+    save_file_name = api_data["save_file_name"]
+    with open(save_file_name, 'wb') as f:
+        f.write(reposne.content)
 
-print("Downloading Taxi zone data!")
-zone_shapes_api = "https://d37ci6vzurychx.cloudfront.net/misc/taxi_zones.zip"
-reposne = requests.get(zone_shapes_api)
-with open("taxi-zones.zip", 'wb') as f:
-    f.write(reposne.content)
-unzip_and_delete("taxi-zones.zip")
+    if "unzip_and_delete" in api_data and api_data["unzip_and_delete"]:
+        unzip_and_delete(save_file_name)
 
-print("Downloading School lications data!")
-school_locations_api = "https://data.cityofnewyork.us/download/jfju-ynrr/application%2Fx-zip-compressed"
-reposne = requests.get(school_locations_api)
-with open("school-locations.zip", 'wb') as f:
-    f.write(reposne.content)
-unzip_and_delete("school-locations.zip")
 
-print("Downloading University locations data!")
-universities_api = "https://data.cityofnewyork.us/api/views/8pnn-kkif/rows.csv?date=20250414&accessType=DOWNLOAD"
-reposne = requests.get(universities_api)
-with open("universities.csv", 'wb') as f:
-    f.write(reposne.content)
+WEATHER_API = {
+    "url" : "https://archive-api.open-meteo.com/v1/archive?latitude=40.73061&longitude=-73.935242&start_date=2012-01-01&end_date=2025-01-01&hourly=temperature_2m,rain,snowfall,snow_depth,wind_speed_10m,wind_gusts_10m&timezone=America%2FNew_York&format=csv",
+    "save_file_name": "weather.csv"
+}
 
-print("Downloading Points of interest data!")
-points_of_interest_api = "https://data.cityofnewyork.us/api/views/t95h-5fsr/rows.csv?date=20250414&accessType=DOWNLOAD"
-reposne = requests.get(points_of_interest_api)
-with open("points-of-interest.csv", 'wb') as f:
-    f.write(reposne.content)
+ZONE_SHAPE_API = {
+    "url" : "https://d37ci6vzurychx.cloudfront.net/misc/taxi_zones.zip",
+    "save_file_name" : "zones_shape.zip",
+    "unzip_and_delete" : True
+}
+
+SCHOOL_LOCATIONS_API = {
+    "url" : "https://data.cityofnewyork.us/download/jfju-ynrr/application%2Fx-zip-compressed",
+    "save_file_name" : "school_locations.zip",
+    "unzip_and_delete" : True
+}
+
+UNIVERSITY_LOCATIONS_API = {
+    "url" : "https://data.cityofnewyork.us/api/views/8pnn-kkif/rows.csv?date=20250414&accessType=DOWNLOAD",
+    "save_file_name" : "university_locations.csv"
+}
+
+POINTS_OF_INTEREST_API = {
+    "url" : "https://data.cityofnewyork.us/api/views/t95h-5fsr/rows.csv?date=20250414&accessType=DOWNLOAD",
+    "save_file_name" : "points_of_interest.csv",
+}
+
+API_DATA = {
+    "weather" : WEATHER_API,
+    "zone" : ZONE_SHAPE_API,
+    "schools" : SCHOOL_LOCATIONS_API,
+    "universities" : UNIVERSITY_LOCATIONS_API,
+    "poi" : POINTS_OF_INTEREST_API
+}
 
 # print("Downloading Event locations data!")
 # event_locations_api = "https://data.cityofnewyork.us/resource/cpcm-i88g.csv?$limit=100000"
@@ -53,3 +62,19 @@ with open("points-of-interest.csv", 'wb') as f:
 # reposne = requests.get(events_api)
 # with open("events.csv", 'wb') as f:
 #     f.write(reposne.content)
+
+if __name__ == "__main__":
+    
+    user_prompt = f"""Input one of the following values to download specific data {"\n".join(API_DATA.keys())} or input 'all' to download everything, input anything else to exit."""
+
+    while True:
+        recieved_input = input(user_prompt)
+
+        if recieved_input in API_DATA:
+            download_from_api(API_DATA[recieved_input])
+        elif recieved_input.lower() == "all":
+            for value in tqdm.tqdm(API_DATA.values()):
+                download_from_api(value)
+            break
+        else:
+            break
--- data/download-taxi-data.py
+++ data/download-taxi-data.py
@@ -1,11 +1,74 @@
 import tqdm, requests, os
 
-url_download = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{}-{:02}.parquet"
-os.mkdir("taxi-data")
+def make_directory(dir_name):
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
 
-for year in tqdm.tqdm(range(2019,2025), desc="Year", position=0):
-    for month in tqdm.tqdm(range(1, 13), desc="Month", position=1):
-        download = url_download.format(year, month)
-        response = requests.get(download)
-        with open(f'taxi-data/{year}-{month:02}.parquet', 'wb') as f:
-            f.write(response.content)
+def download_data_set(api_data):
+    url_download = api_data.get("url")
+    end = api_data.get("end")
+    start = api_data.get("start") 
+    save_location = api_data.get("save_folder") + "/{}-{:02}.parquet"
+
+    make_directory(api_data.get("save_folder"))
+
+    for year in tqdm.tqdm(range(start,end), desc="Year", position=0):
+        for month in tqdm.tqdm(range(1, 13), desc="Month", position=1):
+            download = url_download.format(year, month)
+            response = requests.get(download)
+            with open(save_location.format(year, month), 'wb') as f:
+                f.write(response.content)
+
+YELLOW_TAXI_API = {
+    "url" : "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{}-{:02}.parquet",
+    "save_folder": "taxi-data",
+    "start" : 2012,
+    "end" : 2025
+}
+
+GREEN_TAXI_API = {
+    "url" : "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{}-{:02}.parquet",
+    "save_folder": "green_taxi",
+    "start" : 2014,
+    "end" : 2025
+}
+
+FOR_HIRE_API = {
+    "url" : "https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_{}-{:02}.parquet",
+    "save_folder" : "for_hire",
+    "start" : 2015,
+    "end" : 2025
+}
+
+HIGH_VOLUME_API = {
+    "url" : "https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_{}-{:02}.parquet",
+    "save_folder" : "high_volume",
+    "start" : 2019,
+    "end" : 2025
+}
+
+
+API_DATA = {
+    "yellow-taxi" : YELLOW_TAXI_API,
+    "green-taxi" : GREEN_TAXI_API,
+    "for-hire" : FOR_HIRE_API,
+    "high-volume" : HIGH_VOLUME_API
+}
+
+
+
+if __name__ == "__main__":
+    user_prompt = f"""Input one of the following values to download specific data {"\n".join(API_DATA.keys())}, input anything else to exit."""
+
+    while True:
+        recieved_input = input(user_prompt)
+
+        if recieved_input in API_DATA:
+            download_data_set(API_DATA[recieved_input])
+        elif recieved_input.lower() == "all":
+            for value in tqdm.tqdm(API_DATA.values()):
+                download_data_set(value)
+            break
+        else:
+            break
+
